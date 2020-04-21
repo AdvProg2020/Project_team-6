b9df0d5755fa80005de6ea930b0b9c234f0e7569
GildedJson <sms.tabaian@gmail.com>
2020-04-20 17:08:48 -0700

    added Gson

--- APTeam6/src/main/java/model/account/Buyer.java
+++ APTeam6/src/main/java/model/account/Buyer.java
@@ -1,11 +1,24 @@
 package model.account;
 
+import model.logs.BuyLog;
+
+import java.util.ArrayList;
+
 public class Buyer extends Account {
     private long credit;
+    private ArrayList<BuyLog> buyLogs = new ArrayList<BuyLog>();
 
     public Buyer(String username, String password, String firstName, String lastName, String emailAddress, String phoneNumber) {
         super(username, password, firstName, lastName, emailAddress, phoneNumber);
         this.role = 1;
         credit = 0;
+    }
+
+    public boolean doesHaveEnoughMoney(long price){
+        return credit >= price;
+    }
+
+    public void modifyCreditBy(long amount){
+        credit += amount;
     }
 }
--- APTeam6/src/main/java/model/account/Seller.java
+++ APTeam6/src/main/java/model/account/Seller.java
@@ -1,11 +1,20 @@
 package model.account;
 
+import model.logs.SellLog;
+
+import java.util.ArrayList;
+
 public class Seller extends Account {
     private long credit;
+    private ArrayList<SellLog> sellLogs = new ArrayList<SellLog>();
 
     public Seller(String username, String password, String firstName, String lastName, String emailAddress, String phoneNumber) {
         super(username, password, firstName, lastName, emailAddress, phoneNumber);
         this.role = 2;
         credit = 0;
+    }
+
+    public void modifyCreditBy(long amount){
+        credit += amount;
     }
 }
