db92a687c70580caab81f2dec58d34d720799807
mirkamalice <mirkamali.ce@gmail.com>
2020-05-11 21:40:11 +0430

    add sort method for Manager and Seller

--- APTeam6/src/main/java/model/account/Seller.java
+++ APTeam6/src/main/java/model/account/Seller.java
@@ -1,10 +1,13 @@
 package model.account;
 
+import controller.ProgramManager;
 import model.logs.SellLog;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
-public class Seller extends Account {
+public class Seller extends Account implements Comparable<Seller> {
     private long credit;
     public static ArrayList<Integer> sellLogIds = new ArrayList<Integer>();
     private String companyName;
@@ -18,5 +21,45 @@ public class Seller extends Account {
 
     public void modifyCreditBy(long amount){
         credit += amount;
+    }
+
+    private static int field = 1;
+    private static ArrayList<Seller> sellerArrayList = new ArrayList<>();
+
+    public int compareTo(Seller seller) {
+        if (field == 1) {
+            return -(seller.firstName.compareTo(this.firstName));
+        } else if (field == 2) {
+            return -(seller.lastName.compareTo(this.lastName));
+        } else if (field == 3) {
+            return -(seller.phoneNumber.compareTo(this.phoneNumber));
+        } else if (field == 4) {
+            return -(seller.emailAddress.compareTo(this.emailAddress));
+        } else if (field == 5) {
+            return -(seller.username.compareTo(this.username));
+        } else {
+            return 0;
+        }
+    }
+
+    public ArrayList<Seller> sortSellers(int fieldSort) {
+        /*
+        How to use this method :
+        fieldSort = 1 for firstName sort
+        fieldSort = 2 for lastName sort
+        fieldSort = 3 for phoneNumber sort
+        fieldSort = 4 for emailAddress sort
+        fieldSort = 5 for username sort
+         */
+        field = fieldSort;
+        Collection<Account> values = ProgramManager.getProgramManagerInstance().getAllAccounts().values();
+        ArrayList<Account> listOfValues = new ArrayList<>(values);
+        for (int i = 0; i < listOfValues.size(); i++) {
+            if (listOfValues.get(i).role==2) {
+                sellerArrayList.add((Seller) listOfValues.get(i));
+            }
+        }
+        Collections.sort(sellerArrayList);
+        return sellerArrayList;
     }
 }
