3a47b0b8b733dfa7776f51cc0747f1b5358b0c35
GildedJson <sms.tabaian@gmail.com>
2020-05-14 23:12:43 -0700

    added Category and SubCategory

--- APTeam6/src/main/java/model/product/Category.java
+++ APTeam6/src/main/java/model/product/Category.java
@@ -1,4 +1,35 @@
 package model.product;
 
+import java.util.ArrayList;
+
 public class Category {
+    private static int nextId = 0;
+    private int id;
+    private String name;
+    private ArrayList<SubCategory> subCategories;
+    private ArrayList<String> additionalAttributes;
+
+    public Category(String name){
+        this.name = name;
+        this.id = nextId;
+        nextId++;
+        subCategories = new ArrayList<>();
+        additionalAttributes = new ArrayList<>();
+    }
+
+    public void addField(String field){
+        additionalAttributes.add(field);
+    }
+
+    public void removeField(String field){
+        additionalAttributes.remove(field);
+    }
+
+    public void addSubcategory(SubCategory subCategory){
+        subCategories.add(subCategory);
+    }
+
+    public void removeSubcategory(SubCategory subCategory){
+        subCategories.remove(subCategory);
+    }
 }
--- APTeam6/src/main/java/model/product/SubCategory.java
+++ APTeam6/src/main/java/model/product/SubCategory.java
@@ -1,4 +1,31 @@
 package model.product;
 
+import java.util.ArrayList;
+
 public class SubCategory {
+    private String name;
+    private ArrayList<Product> products;
+    private ArrayList<String> additionalAttributes;
+
+    public SubCategory(String name){
+        this.name = name;
+        products = new ArrayList<>();
+        additionalAttributes = new ArrayList<>();
+    }
+
+    public void addField(String field){
+        additionalAttributes.add(field);
+    }
+
+    public void removeField(String field){
+        additionalAttributes.remove(field);
+    }
+
+    public void addSubcategory(Product product){
+        products.add(product);
+    }
+
+    public void removeSubcategory(Product product){
+        products.remove(product);
+    }
 }
