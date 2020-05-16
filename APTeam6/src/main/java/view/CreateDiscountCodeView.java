26e12291f0d54adaa64c356372830987a4a0a3ca
Zenbook <mohammadali1379@gmail.com>
2020-05-16 19:18:49 +0430

    Changes in CreateDiscountCodeView

--- APTeam6/src/main/java/view/CreateDiscountCodeView.java
+++ APTeam6/src/main/java/view/CreateDiscountCodeView.java
@@ -22,6 +22,28 @@ public class CreateDiscountCodeView {
             }
         }
         }
+    public String[] getUserUsualData() {
+        String[] discountCodeInfo = new String[5];
+        System.out.println("Enter the discountCode:");
+        discountCodeInfo[0] = Input.getInput();
+        System.out.println("Enter the startDate:");
+        discountCodeInfo[1] = Input.getInput();
+        System.out.println("Enter the endDate:");
+        discountCodeInfo[2] = Input.getInput();
+        System.out.println("Enter the percentage:");
+        discountCodeInfo[3] = Input.getInput();
+        System.out.println("Enter the repetition time:");
+        discountCodeInfo[4] = Input.getInput();
+        while (true) {
+            System.out.println("");
+            discountCodeInfo[2] = Input.getInput();
+            if (discountCodeInfo[2].matches("[0-9]+")) {
+                break;
+            }
+            System.out.println("Wrong phone number");
+        }
+        return discountCodeInfo;
+    }
     public void giveOutput(String message){
         System.out.println(message);
     }
