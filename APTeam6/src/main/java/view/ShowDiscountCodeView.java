502378c3f92bf7c2d06dde072b4aa26654b42b12
Zenbook <mohammadali1379@gmail.com>
2020-05-17 23:19:27 +0430

    developing ShowDiscountCode and ShowDiscountCodeView

--- APTeam6/src/main/java/view/ShowDiscountCodeView.java
+++ APTeam6/src/main/java/view/ShowDiscountCodeView.java
@@ -1,6 +1,7 @@
 package view;
 
 import controller.ProgramManager;
+import model.product.DiscountCode;
 
 public class ShowDiscountCodeView {
 
@@ -31,5 +32,19 @@ public class ShowDiscountCodeView {
                 System.out.println("Invalid command");
             }
         }
+    }
+    public void viewDiscountCode(DiscountCode discountCode){
+        System.out.println("the discountCode's ID is " + discountCode.getId());
+        System.out.println("the discountCode's code is " + discountCode.getCode());
+        System.out.println("the discountCode's startDate is " + discountCode.getStart());
+        System.out.println("the discountCode's endDate is " + discountCode.getEnd());
+        System.out.println("the discountCode's percentage is " + discountCode.getPercentage());
+        System.out.println("the discountCode's repetitionTime is " + discountCode.getRepetitionTime());
+    }
+    public void editDiscountCode(DiscountCode discountCode){
+
+    }
+    public void removeDiscountCode(DiscountCode discountCode){
+
     }
 }
