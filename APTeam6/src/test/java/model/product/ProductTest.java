65a2c457ef10f4547ddcb8a5b4e6ae5f3075f6da
mirkamalice <mirkamali.ce@gmail.com>
2020-05-18 23:53:15 +0430

    add product test

--- APTeam6/src/test/java/model/product/ProductTest.java
+++ APTeam6/src/test/java/model/product/ProductTest.java
@@ -1,4 +1,20 @@
 package model.product;
 
+import controller.ProgramManager;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ProductTest {
+
+    @Test
+    public void productTest(){
+        ProgramManager.getProgramManagerInstance().createAllProductHashMap();
+        ProgramManager.getProgramManagerInstance().createAllCategoriesHashMapForTest();
+        Category category = new Category("electrical");
+        SubCategory subCategory = new SubCategory("tv");
+        category.addSubcategory(subCategory);
+        Product product = new Product("tv samsung","electrical","tv");
+        Assert.assertEquals(0,product.getId());
+
+    }
 }
