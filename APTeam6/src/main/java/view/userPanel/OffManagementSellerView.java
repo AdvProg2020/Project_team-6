aa604cefa4b1d0b68d11b0f6de9010a7e6f490f6
Zenbook <mohammadali1379@gmail.com>
2020-05-19 05:00:23 +0430

    OffManagementSellerView Nearly Done!

--- APTeam6/src/main/java/view/userPanel/OffManagementSellerView.java
+++ APTeam6/src/main/java/view/userPanel/OffManagementSellerView.java
@@ -1,4 +1,32 @@
 package view.userPanel;
 
+import controller.ProgramManager;
+import view.Input;
+
 public class OffManagementSellerView {
+    public OffManagementSellerView() {
+        System.out.println("=== OffManagementSellerView menu");
+    }
+
+    public String getInputCommand() {
+        String command;
+        while (true) {
+            command = Input.getInput();
+            if (command.equals("back")) {
+                return command;
+            }
+            else if (command.matches("view \\.+")) {
+                return command;
+            }
+            else if(command.matches("edit \\.+")){
+                return command;
+            }
+            else if(command.equalsIgnoreCase("add off")){
+                return command;
+            }
+            else {
+                System.out.println("Invalid command");
+            }
+        }
+    }
 }
