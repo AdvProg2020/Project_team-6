3459853cedb00e0b5676b9ac0d7fa779858e229a
Zenbook <mohammadali1379@gmail.com>
2020-05-25 21:45:44 +0430

    More Progress in OffRequest

--- APTeam6/src/main/java/model/requests/OffRequest.java
+++ APTeam6/src/main/java/model/requests/OffRequest.java
@@ -1,10 +1,13 @@
 package model.requests;
 
+import controller.ProgramManager;
+import model.product.Off;
+
 public class OffRequest implements Request {
-    //TODO: We need Off first
+    private Off off;
     private byte action;
-    // 0 - create
-    // 1 - change
+    // 0 - add Off
+    // 1 - edit
 
     @Override
     public void accept() {
@@ -13,11 +16,61 @@ public class OffRequest implements Request {
 
     @Override
     public void decline() {
-
+        ProgramManager.getProgramManagerInstance().removeRequest(this);
     }
 
     @Override
     public void showDetails() {
         //TODO
     }
+
 }
+
+/*
+    private Product product;
+    private byte action;
+    private String newValue;
+    // 0 - create
+    // 1 - change name
+    // 2 - change description
+    // 3 - delete
+
+    /**
+     * @param product the product
+     * @param action the action to do.<br/>0 - create<br/>1 - change name<br/>2 - change description<br/>3 - delete<br/>
+     * @param newValue new text for change. null otherwise
+
+/*public ProductRequest(Product product, byte action, String newValue) {
+    this.product = product;
+    this.action = action;
+    this.newValue = newValue;
+}
+
+    @Override
+    public void accept() {
+        if (action == 0){
+            ProgramManager.getProgramManagerInstance().addProductToList(product);
+        }
+        else if (action == 1 || action == 2){
+            product.changeField(action, newValue);
+        }
+        else if (action == 3){
+            ProgramManager.getProgramManagerInstance().removeProduct(product);
+        }
+        ProgramManager.getProgramManagerInstance().removeRequest(this);
+    }
+
+    @Override
+    public void decline() {
+        ProgramManager.getProgramManagerInstance().removeRequest(this);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("the product's name :" + product.getName());
+        System.out.println("the product's Id :" + product.getId());
+        System.out.println("the product's description :" + product.getDescription());
+        System.out.println("the product's categoryName :" + product.getCategoryName());
+    }
+
+ */
