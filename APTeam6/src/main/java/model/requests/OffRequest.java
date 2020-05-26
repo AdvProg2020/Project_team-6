e0f641ca1116aa9a442e62921155cd08eb487759
Zenbook <mohammadali1379@gmail.com>
2020-05-26 23:07:44 +0430

    Finishing OffRequest

--- APTeam6/src/main/java/model/requests/OffRequest.java
+++ APTeam6/src/main/java/model/requests/OffRequest.java
@@ -23,17 +23,10 @@ public class OffRequest implements Request {
     public void accept() {
         if(action == 0){
             ProgramManager.getProgramManagerInstance().addOffToList(off);
-        }else if(action == 1){
-            //TODO
-        }
-        else if(action == 2){
-            //TODO
-        }
-        else if(action == 3){
-            //TODO
-        }
-        else if(action == 4){
-            //TODO
+            ProgramManager.getProgramManagerInstance().removeRequest(this);
+        }else if(action == 1 || action == 2| action == 3| action == 4){
+            off.changeField(action,changingFiled);
+            ProgramManager.getProgramManagerInstance().removeRequest(this);
         }
 
     }
@@ -49,52 +42,3 @@ public class OffRequest implements Request {
     }
 
 }
-
-/*
-    private Product product;
-    private byte action;
-    private String newValue;
-    // 0 - create
-    // 1 - change name
-    // 2 - change description
-    // 3 - delete
-
-    /**
-     * @param product the product
-     * @param action the action to do.<br/>0 - create<br/>1 - change name<br/>2 - change description<br/>3 - delete<br/>
-     * @param newValue new text for change. null otherwise
-
-/*public ProductRequest(Product product, byte action, String newValue) {
-    this.product = product;
-    this.action = action;
-    this.newValue = newValue;
-}
-
-    @Override
-    public void accept() {
-        if (action == 0){
-            ProgramManager.getProgramManagerInstance().addProductToList(product);
-        }
-        else if (action == 1 || action == 2){
-            product.changeField(action, newValue);
-        }
-        else if (action == 3){
-            ProgramManager.getProgramManagerInstance().removeProduct(product);
-        }
-        ProgramManager.getProgramManagerInstance().removeRequest(this);
-    }
-
-    @Override
-    public void decline() {
-        ProgramManager.getProgramManagerInstance().removeRequest(this);
-    }
-
-    @Override
-    public void showDetails() {
-        System.out.println("the product's name :" + product.getName());
-        System.out.println("the product's Id :" + product.getId());
-        System.out.println("the product's description :" + product.getDescription());
-        System.out.println("the product's categoryName :" + product.getCategoryName());
-    }
-
- */
