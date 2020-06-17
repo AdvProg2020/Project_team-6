d6d7abb43e218ccd26d37d2a0f6aabcdfdc6b678
GildedJson <sms.tabaian@gmail.com>
2020-06-17 03:44:57 -0700

    Made Product menu basics and added many TODOs and cried

--- APTeam6/src/main/java/controller/SingleProductScreen.java
+++ APTeam6/src/main/java/controller/SingleProductScreen.java
@@ -1,4 +1,18 @@
 package controller;
 
+import view.SingleProductScreenView;
+
 public class SingleProductScreen {
+    private static SingleProductScreen instance;
+    public static SingleProductScreen getInstance() {
+        if (instance == null)
+            instance = new SingleProductScreen();
+        return instance;
+    }
+    ////////////////////////////////////////
+    private SingleProductScreenView view = new SingleProductScreenView();
+
+    //TODO: Pass the product stuff to view
+    //TODO: add to buyBasket for buyer
+    //TODO: Compare products ToT 😭
 }
