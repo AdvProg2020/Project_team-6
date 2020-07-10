eb8ad35a3b308541b52d32361b3e86de7388c33b
mirkamalice <mirkamali.ce@gmail.com>
2020-07-10 12:03:20 +0430

    edit log

--- APTeam6/src/main/java/server/Log.java
+++ APTeam6/src/main/java/server/Log.java
@@ -8,37 +8,42 @@ import java.time.format.DateTimeFormatter;
 
 public class Log {
 
-    public String fileName = "";
+    private boolean saveLogs = false;
+    private String fileName = "";
 
-    public Log(String name) {
-        this.fileName = name;
-        try {
-            File file = new File("src\\main\\java\\server\\logs\\" + name + ".txt");
-            if (file.createNewFile()) {
-                System.out.println("Log file created: " + file.getName());
-            } else {
-                System.out.println("File already exists.");
+    Log(String name) {
+        if(saveLogs) {
+            this.fileName = name;
+            try {
+                File file = new File("src\\main\\java\\server\\logs\\" + name + ".txt");
+                if (file.createNewFile()) {
+                    System.out.println("Log file created: " + file.getName());
+                } else {
+                    System.out.println("File already exists.");
+                }
+            } catch (IOException e) {
+                System.out.println("An error occurred in logs.");
             }
-        } catch (IOException e) {
-            System.out.println("An error occurred in logs.");
         }
     }
 
-    public void addLog(String log, int mode) {
-        try {
-            FileWriter myWriter = new FileWriter("src\\main\\java\\server\\logs\\" + this.fileName + ".txt", true);
-            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd--HH:mm:ss   ");
-            LocalDateTime now = LocalDateTime.now();
-            if (mode==0) {
-                myWriter.write("send    " + dtf.format(now) + log + "\n");
-            } else if(mode==1){
-                myWriter.write("receive " + dtf.format(now) + log + "\n");
-            }else{
-                myWriter.write("server  " + dtf.format(now) + log + "\n");
+    void addLog(String log, int mode) {
+        if(saveLogs) {
+            try {
+                FileWriter myWriter = new FileWriter("src\\main\\java\\server\\logs\\" + this.fileName + ".txt", true);
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd--HH:mm:ss   ");
+                LocalDateTime now = LocalDateTime.now();
+                if (mode == 0) {
+                    myWriter.write("send    " + dtf.format(now) + log + "\n");
+                } else if (mode == 1) {
+                    myWriter.write("receive " + dtf.format(now) + log + "\n");
+                } else {
+                    myWriter.write("server  " + dtf.format(now) + log + "\n");
+                }
+                myWriter.close();
+            } catch (IOException e) {
+                System.out.println("An error occurred in logs.");
             }
-            myWriter.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred in logs.");
         }
     }
 }
