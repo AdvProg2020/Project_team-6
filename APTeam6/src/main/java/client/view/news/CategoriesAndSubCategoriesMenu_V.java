676575af3b13dac443df960b73e61b9ece46d4f1
GildedJson <sms.tabaian@gmail.com>
2020-07-14 23:43:17 -0700

    Added message TODOs to CategoriesAndSubCategoriesMenu_V

--- APTeam6/src/main/java/client/view/news/CategoriesAndSubCategoriesMenu_V.java
+++ APTeam6/src/main/java/client/view/news/CategoriesAndSubCategoriesMenu_V.java
@@ -2,28 +2,40 @@ package client.view.news;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 
 public class CategoriesAndSubCategoriesMenu_V extends GeneralController_V{
+    //This method is to be called before setting the scene
+    public void reset(){
+        listView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
     public ListView listView;
     public ListView attributesListView;
 
     public void backBtn(ActionEvent actionEvent) {
-
+        //TODO: (message) -
     }
 
     public void addBtn(ActionEvent actionEvent) {
-
+        //TODO: (message) -
     }
 
     public void removeBtn(ActionEvent actionEvent) {
-
+        if (listView.getSelectionModel().getSelectedIndices().size() == 1){
+            //TODO: (message) index
+        }
     }
 
     public void editBtn(ActionEvent actionEvent) {
-
+        if (listView.getSelectionModel().getSelectedIndices().size() == 1){
+            //TODO: (message) index
+        }
     }
 
     public void openBtn(ActionEvent actionEvent) {
-
+        if (listView.getSelectionModel().getSelectedIndices().size() == 1){
+            //TODO: (message) index
+        }
     }
 }
