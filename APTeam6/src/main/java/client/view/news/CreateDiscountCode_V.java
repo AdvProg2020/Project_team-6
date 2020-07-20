fa7ea157e1382b13a1dbe223385d6172d30b16b6
GildedJson <sms.tabaian@gmail.com>
2020-07-19 17:03:57 -0700

    added regex checks to CreateDiscountCode_V

--- APTeam6/src/main/java/client/view/news/CreateDiscountCode_V.java
+++ APTeam6/src/main/java/client/view/news/CreateDiscountCode_V.java
@@ -1,6 +1,7 @@
 package client.view.news;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -14,7 +15,15 @@ public class CreateDiscountCode_V {
     public ListView<CheckBox> buyersListView;
 
     public void createButton(ActionEvent actionEvent) {
-        //TODO: (Message) everything
+        if (codeText.getText().equals("") || daysBeforeStartText.getText().equals("") || daysBeforeEndText.getText().equals("") || percentageText.getText().equals("") || repetitionText.getText().equals("")){
+            new Alert(Alert.AlertType.ERROR, "No empty fields!").showAndWait();
+        }
+        else if (!codeText.getText().matches("[0-9]+") || !daysBeforeStartText.getText().matches("[0-9]+") || !daysBeforeEndText.getText().matches("[0-9]+") || !percentageText.getText().matches("[0-9]+") || !repetitionText.getText().matches("[0-9]+")){
+            new Alert(Alert.AlertType.ERROR, "Wrong input!").showAndWait();
+        }
+        else {
+            //TODO: (Message) everything
+        }
     }
 
     public void cancelButton(ActionEvent actionEvent) {
