375152e7618196fda38952e07aa567dfb3737675
GildedJson <sms.tabaian@gmail.com>
2020-07-21 18:19:32 -0700

    Finalize LoadingScreen_V

--- APTeam6/src/main/java/client/view/news/LoadingScreen_V.java
+++ APTeam6/src/main/java/client/view/news/LoadingScreen_V.java
@@ -2,9 +2,17 @@ package client.view.news;
 
 import javafx.scene.control.ProgressBar;
 
-import java.util.Timer;
-
 public class LoadingScreen_V extends GeneralController_V{
-    public ProgressBar LoadProgress;
-    Timer timer = new Timer();
+    public ProgressBar loadProgress;
+    public void initialize(){
+        for (int i = 0; i < 100; i++) {
+            loadProgress.setProgress(i);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        //TODO: test if this works and then call main screen from here
+    }
 }
