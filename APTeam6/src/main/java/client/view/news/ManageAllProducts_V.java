b47e3f0fc6e766138968d550cd220afbb4324a29
GildedJson <sms.tabaian@gmail.com>
2020-07-22 03:12:39 -0700

    Finished ManageRequests_V and did some more things

--- APTeam6/src/main/java/client/view/news/ManageAllProducts_V.java
+++ APTeam6/src/main/java/client/view/news/ManageAllProducts_V.java
@@ -1,8 +1,16 @@
 package client.view.news;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 
 public class ManageAllProducts_V {
+    public ListView<String> productsListView;
+
+    public void initialize(){
+        productsListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
     public void backButton(ActionEvent actionEvent) {
         //TODO: (message) -
     }
---
+++ APTeam6/src/main/java/client/view/news/ManageRequests_V.java
@@ -0,0 +1,34 @@
+package client.view.news;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+
+public class ManageRequests_V {
+    public ListView<String> requestListView;
+    public void initialize(){
+        requestListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
+    public void acceptButton(ActionEvent actionEvent) {
+        if (requestListView.getSelectionModel().getSelectedIndices().size() == 1){
+            //TODO: (message) index
+        }
+    }
+
+    public void declineButton(ActionEvent actionEvent) {
+        if (requestListView.getSelectionModel().getSelectedIndices().size() == 1){
+            //TODO: (message) index
+        }
+    }
+
+    public void detailsButton(ActionEvent actionEvent) {
+        if (requestListView.getSelectionModel().getSelectedIndices().size() == 1){
+            //TODO: (message) index
+        }
+    }
+
+    public void backButton(ActionEvent actionEvent) {
+        //TODO: (message) index
+    }
+}
