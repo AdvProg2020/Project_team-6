419fa15ff938b000fedeac094a2b4a66462481c5
GildedJson <sms.tabaian@gmail.com>
2020-07-21 19:57:27 -0700

    Made the MainScreen_V and added TODO to LoadingScreen_V

--- APTeam6/src/main/java/client/view/news/MainScreen_V.java
+++ APTeam6/src/main/java/client/view/news/MainScreen_V.java
@@ -1,4 +1,21 @@
 package client.view.news;
 
+import javafx.event.ActionEvent;
+
 public class MainScreen_V extends GeneralController_V{
+    public void userPanelButton(ActionEvent actionEvent) {
+        //TODO: (message) -
+    }
+
+    public void productsButton(ActionEvent actionEvent) {
+        //TODO: (message) -
+    }
+
+    public void offsButton(ActionEvent actionEvent) {
+        //TODO: (message) -
+    }
+
+    public void loginMenuButton(ActionEvent actionEvent) {
+        //TODO: (message) -
+    }
 }
