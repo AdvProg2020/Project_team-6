95521c865fb76c505de83c4ac6186fc0380402c8
Zenbook <mohammadali1379@gmail.com>
2020-07-24 18:20:35 +0430

    ShowCart_V.fxml

--- APTeam6/src/main/java/client/view/news/ShowCartـV.java
+++ APTeam6/src/main/java/client/view/news/ShowCartـV.java
@@ -2,10 +2,35 @@ package client.view.news;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class ShowCartـV {
-    public Button send;
+    public Button decrease;
+    public Button increase;
+    public Button viewProduct;
+    public Button showTotalPrice;
+    public Button purchase;
+    public TextField productId;
+
+
     public void send(ActionEvent actionEvent) {
 
     }
+
+    public void increase(ActionEvent actionEvent) {
+        //TODO
+    }
+    public void decrease(ActionEvent actionEvent) {
+        //TODO
+    }
+    public void showTotalPrice(ActionEvent actionEvent) {
+        //TODO
+    }
+    public void viewProduct(ActionEvent actionEvent) {
+        //TODO
+    }
+    public void purchase(ActionEvent actionEvent) {
+        //TODO
+    }
+    
 }
