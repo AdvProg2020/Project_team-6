3460531ce125dc72063223b501b7430bb3df9fc4
Zenbook <mohammadali1379@gmail.com>
2020-07-24 20:10:37 +0430

    More Progress in ShowCart_V.fxml

--- APTeam6/src/main/java/server/controller/SingleProductScreen.java
+++ APTeam6/src/main/java/server/controller/SingleProductScreen.java
@@ -3450,7 +3450,222 @@ public class SingleProductScreen {
 //        if (allOffs == null)
 //            allOffs = new HashMap<>();
 //    }
-//
+////    public void saveToFiles() {
+////        YaGson gsonCreator = new YaGson();
+////        try {
+////            FileWriter fileWriter = new FileWriter(accountsFile, false);
+////            fileWriter.write(gsonCreator.toJson(allAccounts));
+////            fileWriter.close();
+////            fileWriter = new FileWriter(logsFile, false);
+////            fileWriter.write(gsonCreator.toJson(allLogs));
+////            fileWriter.close();
+////            fileWriter = new FileWriter(productsFile, false);
+////            fileWriter.write(gsonCreator.toJson(allProducts));
+////            fileWriter.close();
+////            fileWriter = new FileWriter(categoriesFile, false);
+////            fileWriter.write(gsonCreator.toJson(allCategories));
+////            fileWriter.close();
+////            fileWriter = new FileWriter(discountCodesFile, false);
+////            fileWriter.write(gsonCreator.toJson(allDiscountCodes));
+////            fileWriter.close();
+////            fileWriter = new FileWriter(requestsFile, false);
+////            fileWriter.write(gsonCreator.toJson(allRequests));
+////            fileWriter.close();
+////            fileWriter = new FileWriter(offsFile, false);
+////            fileWriter.write(gsonCreator.toJson(allOffs));
+////            fileWriter.close();
+////        } catch (Exception ignored) {
+////            System.out.println("Failed to save files...");
+////        }
+////    }
+////
+////    //////////////////////////////////////////////
+////
+////    public Collection<LogsInGeneral> getAllLogs() {
+////        return allLogs.values();
+////    }
+////
+////    public HashMap<Integer,LogsInGeneral> getLogsInGeneralHashMap(){
+////        return allLogs;
+////    }
+////
+////    public boolean isThereAccountWithUsername(String name) {
+////        return allAccounts.get(name) != null;
+////    }
+////
+////    public Account getAccountByUsername(String username) {
+////        return allAccounts.get(username);
+////    }
+////
+////    /*public void loginSuccessful(Account account) {
+////        currentlyLoggedInUsers = account;
+////        if (account.getRole() == 1) {
+////            ((Buyer) account).addProductToBuyBasket(buyBasket);
+////            buyBasket.clear();
+////        }
+////    }*/
+////
+////    /*public boolean isAnyoneLoggedIn() {
+////        return currentlyLoggedInUsers != null;
+////    }
+////
+////    public void logoutSuccessful() {
+////        currentlyLoggedInUsers = null;
+////    }
+////
+////     */
+////
+////    public void addAccountToList(String username, Account account) {
+////        allAccounts.put(username, account);
+////    }
+////
+////    public void addAccountToList(Account account) {
+////        allAccounts.put(account.getUsername(), account);
+////    }
+////
+////    public void addLogToList(LogsInGeneral log) {
+////        allLogs.put(log.getLogId(), log);
+////    }
+////
+////    public LogsInGeneral getLogByLogId(int id) {
+////        return allLogs.get(id);
+////    }
+////
+////    public void removeProduct(int productId) {
+////        allProducts.remove(productId);
+////        Product product = allProducts.get(productId);
+////        allCategories.get(product.getCategoryName()).getSubCategoryByName(product.getSubCategoryName()).removeProduct(productId);
+////    }
+////
+////    public void removeProduct(Product product) {
+////        allProducts.remove(product.getId());
+////        allCategories.get(product.getCategoryName()).getSubCategoryByName(product.getSubCategoryName()).removeProduct(product.getId());
+////    }
+////
+////    /**
+////     * this method adds the product to productsList and adds it to its subCategory as well
+////     */
+////    public void addProductToList(Product product) {
+////        try {
+////            allCategories.get(product.getCategoryName()).getSubCategoryByName(product.getSubCategoryName()).addProduct(product.getId());
+////            allProducts.put(product.getId(), product);
+////        } catch (Exception e) {
+////            System.out.println("Fake address");
+////        }
+////    }
+////
+////    public void addOffToList(Off off) {
+////        allOffs.put(off.getOffId(), off);
+////    }
+////
+////    public Product getProductById(int id) {
+////        return allProducts.get(id);
+////    }
+////
+////    public void deleteDiscountCode(DiscountCode discountCode) {
+////        allDiscountCodes.remove(discountCode);
+////    }
+////
+////    public void deleteAccount(String username) {
+////        allAccounts.remove(username);
+////    }
+////
+////    /*public Account getCurrentlyLoggedInUsers() {
+////        return currentlyLoggedInUsers;
+////    }
+////
+////     */
+////
+////
+////     * this function gets current user's role
+////     *
+////     * @return 0 if no one is logged in
+////
+////    /*public byte getCurrentlyLoggedInUserRole() {
+////        if (currentlyLoggedInUsers != null)
+////            return currentlyLoggedInUsers.getRole();
+////        return 0;
+////    }
+////
+////
+////
+////    public Collection<Account> getAllAccounts() {
+////        return allAccounts.values();
+////    }
+////
+////    public ArrayList<Request> getAllRequests() {
+////        return allRequests;
+////    }
+////
+////    public Collection<Category> getAllCategories() {
+////        return allCategories.values();
+////    }
+////
+////
+////
+////    public LocalDateTime parsingStringToDate(String input) {
+////        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+////        LocalDateTime dateTime = LocalDateTime.parse(input, formatter);
+////        return dateTime;
+////    }
+////
+////    public void addDiscountCodeToArrayList(DiscountCode discountCode) {
+////        allDiscountCodes.put(discountCode.getCode(), discountCode);
+////    }
+////
+////    public DiscountCode getDiscountCodeByCode(String code) {
+////        return allDiscountCodes.get(code);
+////    }
+////
+////    public HashMap<String, DiscountCode> getAllDiscountCodes() {
+////        return allDiscountCodes;
+////    }
+////    public HashMap<Integer,Product> getAllProducts() {
+////        return allProducts;
+////    }
+////
+////    /**
+////     * If a buyer is logged in, this method will add the product to their buyBasket, otherwise it will be added to ProgramManager buyBasket.
+////     *
+////     *  product the product to be added
+////     */
+////    /*public void addToCurrentBuyBasket(Product product, int count) {
+////        if (currentlyLoggedInUsers == null) {
+////            if (buyBasket.containsKey(product)) {
+////                count += buyBasket.get(product);
+////                buyBasket.replace(product, count);
+////            } else
+////                buyBasket.put(product, count);
+////        } else if (currentlyLoggedInUsers.getRole() == 1)
+////            ((Buyer) currentlyLoggedInUsers).addProductToBuyBasket(product, count);
+////    }
+////
+////     */
+////
+////    public void addRequestToList(Request request) {
+////        allRequests.add(request);
+////    }
+////
+////    public void removeRequest(Request request) {
+////        allRequests.remove(request);
+////    }
+////
+////
+////    // MKH don't print things here
+////    public String showAllRequests() {
+////        StringBuilder result = new StringBuilder();
+////        result.append("Requests : \n");
+////
+////        for (int i = 0; i < allRequests.size(); i++) {
+////            if (allRequests.get(i) instanceof ProductRequest) {
+////                //System.out.println(i + ". " + allRequests.get(i) + "is a ProductRequest");
+////                result.append("\n").append(i).append(". ").append(allRequests.get(i).toString()).append("is a ProductRequest");
+////            } else if (allRequests.get(i) instanceof OffRequest) {
+////                //System.out.println(i + ". " + allRequests.get(i) + "is an OffRequest");
+////                result.append("\n").append(i).append(". ").append(allRequests.get(i).toString()).append("is an OffRequest");
+////            }
+////        }
+////
 //    public void saveToFiles() {
 //        YaGson gsonCreator = new YaGson();
 //        try {
